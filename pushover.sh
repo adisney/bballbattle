22bc719b9b199dd0cfe54a8a8eb4d35eb1adb43e
adisney <adisney2@gmail.com>
2014-03-27 10:57:12 -0500

    adding pushover notification script

---
+++ pushover.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+curl -s -F "token=ayeuCc7bvufRbLto3G9A6sUEbcMKzu" -F "user=uah7sDAHUfvaju3S4WvALuQK2fq5H5" -F "message=BBall Battle server died" https://api.pushover.net/1/messages.json
