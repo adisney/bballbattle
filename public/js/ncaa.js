8d7559cadb834914ae820ca91028f6e64c888ecb
adisney <adisney2@gmail.com>
2014-04-04 07:33:00 -0500

    added logo div and on click between details and logos

--- public/js/ncaa.js
+++ public/js/ncaa.js
@@ -1,3 +1,37 @@
+function markIfEliminated(elem, teamData) {
+  if (teamData.knocked_out === true) {
+    elem.addClass("knocked-out");
+  }
+}
+
+function populateTeamDetails(row, team, index, teamData) {
+  teamDiv = row.find(".team" + (index + 1));
+  teamDiv.append(teamData.seed + ". " + team);
+  markIfEliminated(teamDiv, teamData);
+  teamDiv.find(".team-logo").attr("src", teamData.logo_url);
+  return row.find(".details");
+}
+
+function populateTeamLogos(row, teamData) {
+  imgTag = row.find(".logos > .template").clone();
+  imgTag.removeClass("template").removeClass("hidden");
+  imgTag.attr("src", teamData.logo_url);
+  markIfEliminated(imgTag, teamData)
+  row.find(".logos > .row-fluid").append(imgTag);
+  return row.find(".logos");
+}
+
+function configOnClick(details, logos) {
+  details.click(function() {
+    details.addClass('hidden');
+    logos.removeClass('hidden');
+  });
+  logos.click(function() {
+    details.removeClass('hidden');
+    logos.addClass('hidden');
+  });
+}
+
 function on_success(response) {
   $(".update-time").text(response.update_time);
 
@@ -5,16 +39,17 @@ function on_success(response) {
   _.each(response.player_picks, function(data, player) {
     row = $(".template").clone().removeClass("template");
     row.find(".player").append(player);
+
     index = 0
     _.each(data.picks, function(teamData, team) {
-      teamDiv = row.find(".team" + (index + 1));
-      teamDiv.append(teamData.seed + ". " + team);
-      if (teamData.knocked_out === true) {
-        teamDiv.addClass("knocked-out");
-      }
-      teamDiv.find(".team-logo").attr("src", teamData.logo_url)
+      details = populateTeamDetails(row, team, index, teamData);
+      logos = populateTeamLogos(row, teamData);
+
+      configOnClick(details, logos);
+
       index += 1;
     });
+
     row.find(".total").append(data.total);
     rows.push(row);
   });
