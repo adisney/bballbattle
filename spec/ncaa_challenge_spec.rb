2233f3409e01247f4e31c15b36dd0898132370ed
Alex Disney <alexdisney@gmail.com>
2015-02-15 20:38:43 -0600

    removed obsolete tests

--- spec/ncaa_challenge_spec.rb
+++ spec/ncaa_challenge_spec.rb
@@ -9,28 +9,7 @@ describe "challenge" do
     expect(details[:player_picks].keys).to eq ["Mark", "Alex D", "Mike E", "Molly D", "Kyle", "Kevin M.", "Gene C.", "Ryan J", "Rob T", "Jon P", "George", "Sam"]
   end
 
-  it "correctly determines player picks" do
-    expect(player_picks["Alex D"][:picks].keys).to eq (["Louisville", "Michigan State", "Harvard", "UCLA", "Connecticut", "Kentucky", "Pittsburgh", "Ohio State"])
-  end
-
   it "sums player score" do
     expect(player_picks["Alex D"][:total]).to eq 28
-  end
-
-  describe "team details" do
-    let (:pick_info) { player_picks["Alex D"][:picks] }
-
-    it "gets team seed" do
-      expect(pick_info["Louisville"][:seed]).to eq "4"
-    end
-
-    it "gets local logo url" do
-      expect(pick_info["Louisville"][:logo_url]).to eq "img/louisville.70.png" 
-    end
-
-    it "determines if team is knocked out" do
-      expect(pick_info["Louisville"][:knocked_out]).to eq false 
-      expect(pick_info["Ohio State"][:knocked_out]).to eq true 
-    end
   end
 end
