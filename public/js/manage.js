37285802a2e12c4f7dfcf47a3dbd6d429085e409
Alex Disney <alexdisney@gmail.com>
2015-03-16 08:14:23 -0500

    trim extra spaces between words in username

--- public/js/manage.js
+++ public/js/manage.js
@@ -3,7 +3,7 @@ function onManage() {
   var teams;
 
   function user() {
-    return $.cookie('username')
+    return $.cookie('username');
   }
   function alreadyLoggedIn() {
     return user();
@@ -113,7 +113,7 @@ function onManage() {
   function onUnregistered() {
     var form = $('#templates .log-in-form').clone();
     form.find('.log-in-btn').click(function() {
-      var name = form.find('.username').val().trim();
+      var name = form.find('.username').val().trim().replace(/\s+/g, " ");
       if(name) {
         $.cookie('username', name, {expires: 90, path:'/'});
         window.location.reload();
